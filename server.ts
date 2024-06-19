4a316e809a544dc03a8f058eedc42d88d5ba23b2
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-06-19 17:46:01 +0200

    added authorization and created POST request for login, GET books request

---
+++ server.ts
@@ -0,0 +1,50 @@
+import express, { json } from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import { toToken } from "./auth/jwt";
+import { AuthRequest } from "./auth/middelware";
+import { z } from "zod";
+
+const app = express();
+app.use(cors());
+const port = 3001;
+
+const prisma = new PrismaClient();
+
+app.use(json());
+
+app.get("/books", async (req, res) => {
+  try {
+    const allBooks = await prisma.book.findMany();
+    res.send(allBooks);
+  } catch (error) {
+    res.status(500).send({ message: "Something went wrong!" });
+  }
+});
+
+app.post("/login", async (req: AuthRequest, res) => {
+  const bodyFromRequest = req.body;
+  if ("username" in bodyFromRequest && "password" in bodyFromRequest) {
+    try {
+      const userToLogin = await prisma.user.findUnique({
+        where: {
+          username: bodyFromRequest.username,
+        },
+      });
+      if (userToLogin && userToLogin.password === bodyFromRequest.password) {
+        const token = toToken({ userId: userToLogin.id });
+        res.status(200).send({ token: token });
+        return;
+      }
+      res.status(400).send({ message: "Login failed" });
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong!" });
+    }
+  } else {
+    res
+      .status(400)
+      .send({ message: "'username' and 'password' are required!" });
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
