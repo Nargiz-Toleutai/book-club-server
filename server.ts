33793c8b169bc5012e1a0ef7266d659fa177287a
Nargiz-Toleutai <nargizabdullinova@gmail.com>
2024-06-19 17:48:02 +0200

    added validation and POST register request

--- server.ts
+++ server.ts
@@ -13,6 +13,17 @@ const prisma = new PrismaClient();
 
 app.use(json());
 
+const UserDataValidator = z
+  .object({
+    username: z.string().min(5, {
+      message: "Username should have a minimum length of 5 characters",
+    }),
+    password: z.string().min(10, {
+      message: "Password should have a minimum length of 10 characters",
+    }),
+  })
+  .strict();
+
 app.get("/books", async (req, res) => {
   try {
     const allBooks = await prisma.book.findMany();
@@ -44,6 +55,28 @@ app.post("/login", async (req: AuthRequest, res) => {
     res
       .status(400)
       .send({ message: "'username' and 'password' are required!" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const bodyFromReq = req.body;
+  const validated = UserDataValidator.safeParse(bodyFromReq);
+
+  if (validated.success) {
+    try {
+      const newUser = await prisma.user.create({
+        data: {
+          username: validated.data.username,
+          password: validated.data.password,
+        },
+      });
+      res.status(201).send({ message: "User created", user: newUser });
+    } catch (error) {
+      console.error("Error creating user:", error);
+      res.status(500).send({ error: "Something went wrong" });
+    }
+  } else {
+    res.status(400).send(validated.error.flatten());
   }
 });
 
